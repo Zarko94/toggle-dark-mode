73ce0bf9d804014505aa7a67fc74c7f8475e3e1b
[zarko milanovic] <[zarko9zaka4@yahoo.com]>
2022-06-14 08:41:26 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Header from "./components/Header";
-import Meme from "./components/Meme";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode((prevState) => !prevState);
+  };
   return (
-    <div>
-      <Header />
-      <Meme />
+    <div className="container">
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
